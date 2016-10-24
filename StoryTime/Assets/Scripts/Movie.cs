24a589bf285172b2ad0e63b96fff349810e7e5f5
cnymose <cnymose@gmail.com>
2016-10-24 08:00:02 +0200

    Hologram

---
+++ StoryTime/Assets/Scripts/Movie.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movie : MonoBehaviour {
+    public MovieTexture movie;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.mainTexture = movie as MovieTexture;
+        movie.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
