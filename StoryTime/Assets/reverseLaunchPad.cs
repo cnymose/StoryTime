0657bf0e3d27e25cadc81741cd603c9263895564
Prygl <lrni13@student.aau.dk>
2016-12-01 14:28:42 +0100

    Bunker placement fix

---
+++ StoryTime/Assets/reverseLaunchPad.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reverseLaunchPad : MonoBehaviour {
+
+    Animator anim;
+    bool reset = false;
+	
+    // Use this for initialization
+	void Start () {
+
+        anim = GetComponentInParent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (!reset && other.tag == "Player")
+        {
+
+            anim.SetBool("isOffPad", true);
+            //AnimationClip clip = transform.parent.FindChild("Sphere").GetComponent<padCollision>().GetAnimationClip("toOrigin");
+        }
+    }
+}
