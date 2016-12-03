ecda24bf3996810dadbeabdb540832e7ce5d29af
cnymose <cnymose@gmail.com>
2016-12-03 18:48:27 +0100

    Stasispod

---
+++ StoryTime/Assets/Scripts/StasisPod.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StasisPod : MonoBehaviour {
+    Player player;
+    float spawnTime;
+    Animator anim;
+    public float waitTime;
+    bool opened = false;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        spawnTime = Time.time;
+        player = GameObject.Find("Player").GetComponent<Player>();
+        StartCoroutine(Open());
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+            
+        
+	}
+    IEnumerator Open() {
+        yield return new WaitForSeconds(waitTime);
+        anim.SetBool("Open", true);
+        
+        yield break;
+    }
+}
